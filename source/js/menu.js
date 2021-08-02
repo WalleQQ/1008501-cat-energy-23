088336794ab8a5bd2c54044aaced2d207ef4f413
Виталий <walleqq@gmail.com>
2021-08-02 15:53:34 +0400

    Добавляю правки стилизации страниц

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,8 +1,9 @@
 const openMenu = document.querySelector(".main-nav__toggle");
+const closeMenu = document.querySelector(".main-nav__toggle-item");
 const menu = document.querySelector(".main-nav__menu");
 
 openMenu.addEventListener("click", function (evt) {
   evt.preventDefault();
-  menu.classList.toggle("show-menu");
-  openMenu.classList.toggle("close-menu");
+  menu.classList.toggle("main-nav__menu--show-menu");
+  closeMenu.classList.toggle("main-nav__toggle-item--close-menu");
 });
